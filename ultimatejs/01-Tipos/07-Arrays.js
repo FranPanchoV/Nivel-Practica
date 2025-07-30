ee998566b2f545cca9ec3371bfc7554697f4891c
FranPanchoV <favelazquez@gmail.com>
2025-07-30 19:53:07 -0400

    js

---
+++ ultimatejs/01-Tipos/07-Arrays.js
@@ -0,0 +1,14 @@
+let animales = ['gato', 'caballo'];
+
+console.log(animales);
+console.log(animales[0]);
+
+animales[2] = 'pez'
+console.log(animales);
+
+animales[10] = 'perro';
+console.log(animales[6]);
+
+console.log((typeof animales));
+console.log(animales.length);
+
---
+++ ultimatejs/01-Tipos/08-Funciones.js
@@ -0,0 +1,14 @@
+function saludar() {
+    console.log('Hola Mundo');
+}
+
+saludar();
+
+function suma() {
+    return 6 + 210;
+}
+
+suma()
+
+// let resultado = suma();
+console.log(suma());
